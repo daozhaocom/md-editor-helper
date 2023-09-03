fa47e638bb283f5cc89b0af90ea7b10b5d9638d0
shadowprompt <huixiong.cn@gmail.com>
2023-09-03 12:52:04 +0800

    update markdown setting

--- src/extension.ts
+++ src/extension.ts
@@ -4,19 +4,31 @@ import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+
+function init() {
+	vscode.workspace.getConfiguration().update("markdown.styles", [ "https://socialsimulation.net/css/post.min.css"], vscode.ConfigurationTarget.Workspace).then(() => {
+		vscode.window.showInformationMessage('Ready md-editor-helper!');
+	});
+}
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "md-editor-helper" is now active!');
+	vscode.window.showInformationMessage('md-editor-helper is now active!');
+
+	init();
+
+	// const configuredView = vscode.workspace.getConfiguration().get('conf.view.showOnWindowOpen');
+	// console.log('configuredView', configuredView);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('md-editor-helper.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('md-editor-helper.helloWorld', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from md-editor-helper!');
+		init();
 	});
 
 	context.subscriptions.push(disposable);
